099fa9adfe7702d2d61359918dc849e219476fdb
PROFFELIP3 <168194026+PROFFELIP3@users.noreply.github.com>
2024-11-14 14:58:26 -0300

    perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -11,13 +11,13 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+     'chamada ou tema',
+    'pergunta?',
+    'resposta'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'chamada ou tema',
+    'pergunta?',
+    'resposta'
 )
