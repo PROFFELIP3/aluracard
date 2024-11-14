cb13c23a20fa9169d6516e64c2a3a765c64aa2ef
PROFFELIP3 <168194026+PROFFELIP3@users.noreply.github.com>
2024-11-14 14:48:24 -0300

    perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,13 +1,13 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'adivinhas',
+    'Qual a parte do corpo que mais coça',
+    'a unha!'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'quinta série ataca novamente',
+    'Qual a mãe mais brava do mundo?',
+    'a eletricidade'
 )
 
 criaCartao(
